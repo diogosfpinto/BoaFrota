cfe6459a4fd311611a725b44325cc074c6190590
diogosfpinto <diogo.sfpinto@gmail.com>
2021-10-16 18:18:22 -0400

    criada classe modelo para Veículo: Vehicle.java

--- src/main/java/com/frotas/FirstProject/model/Vehicle.java
+++ src/main/java/com/frotas/FirstProject/model/Vehicle.java
@@ -1,6 +1,76 @@
 package com.frotas.FirstProject.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Vehicle {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String brand;
+    private String model;
+    private String plate;
+    private int year;
+    private String color;
+    private String Renavam;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getRenavam() {
+        return Renavam;
+    }
+
+    public void setRenavam(String renavam) {
+        Renavam = renavam;
+    }
 }
