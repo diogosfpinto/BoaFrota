37da0a3dc3221b857d888043b5a0733ed62fd942
diogosfpinto <diogo.sfpinto@gmail.com>
2021-10-18 18:42:50 -0400

    criado endpoint para buscar veículo por ID.

--- src/main/java/com/frotas/FirstProject/controller/VehicleController.java
+++ src/main/java/com/frotas/FirstProject/controller/VehicleController.java
@@ -4,6 +4,7 @@ import com.frotas.FirstProject.model.Vehicle;
 import com.frotas.FirstProject.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,6 +26,14 @@ public class VehicleController {
     @GetMapping(path="/all")
     public @ResponseBody Iterable<Vehicle> getAllVehicles(){
         return vehicleRepository.findAll();
+    }
+
+    @GetMapping(path = "/vehicles/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Vehicle> getVehicleById(@PathVariable (value = "id") Integer id){
+        return vehicleRepository.findById(id)
+                .map(vehicle -> ResponseEntity.ok().body(vehicle))
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
