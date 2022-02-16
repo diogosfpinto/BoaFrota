567bef854baf7eae623450b34161150dfd2de32d
diogosfpinto <diogo.sfpinto@gmail.com>
2022-02-16 10:11:28 -0400

    Adicionado Swagger ao projeto;

--- src/main/java/com/frotas/FirstProject/controller/MaintenanceController.java
+++ src/main/java/com/frotas/FirstProject/controller/MaintenanceController.java
@@ -2,6 +2,9 @@ package com.frotas.FirstProject.controller;
 
 import com.frotas.FirstProject.model.Maintenance;
 import com.frotas.FirstProject.repository.MaintenanceRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -16,6 +19,12 @@ public class MaintenanceController {
     @Autowired
     private MaintenanceRepository maintenanceRepository;
 
+    @ApiOperation("Inserindo uma manutenção para o veículo selecionado")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Manutenção inserida com sucesso"),
+            @ApiResponse(code = 500,
+                    message = "Houve um erro ao inserir a manutenção, verifique as informações.")
+    })
     @PostMapping("/maintenance")
     @ResponseStatus(HttpStatus.CREATED)
     public @ResponseBody String addNewMaintenance(@PathVariable (value = "id") Integer id,
@@ -25,6 +34,7 @@ public class MaintenanceController {
     }
 
     //        Como buscar as manutenções de um veículo em específico
+    @ApiOperation("Retornando todos as manutenções para o veículo selecionado")
     @GetMapping(path="/maintenances")
     @ResponseStatus(HttpStatus.OK)
     public @ResponseBody List<Maintenance> getAllMaintenancesByIdVehicle
