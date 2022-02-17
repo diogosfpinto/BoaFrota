55b3fe87f5dccef6d0fde0529c501d88cf409cae
diogosfpinto <diogo.sfpinto@gmail.com>
2022-02-17 10:43:56 -0400

    Separando  camada de aplicação UserController e UserService

--- src/main/java/com/frotas/FirstProject/controller/UserController.java
+++ src/main/java/com/frotas/FirstProject/controller/UserController.java
@@ -1,11 +1,10 @@
 package com.frotas.FirstProject.controller;
 
 import com.frotas.FirstProject.model.User;
-import com.frotas.FirstProject.repository.UserRepository;
+import com.frotas.FirstProject.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,31 +12,22 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @PostMapping(path="/user")
     @ResponseStatus(HttpStatus.CREATED)
-    public @ResponseBody String addNewUser(@RequestBody User user){
-        String pws = user.getPassword();
-        user.setPassword(new BCryptPasswordEncoder().encode(pws));
-        userRepository.save(user);
-        return "saved";
+    public @ResponseBody User addNewUser(@RequestBody User user){
+        return userService.addNewUser(user);
     }
 
     @GetMapping(path="/users")
     public @ResponseBody Iterable<User> getAllUser(){
-        return userRepository.findAll();
+        return userService.getAllUsers();
     }
 
-    /**
-     * @param id id
-     * @return User by Id
-     */
     @GetMapping(path="/users/{id}")
     public ResponseEntity<User> getUserById(@PathVariable (value = "id") Integer id){
-        return userRepository.findById(id).map(
-                user -> ResponseEntity.ok().body(user))
-                .orElse(ResponseEntity.notFound().build());
+        return userService.getUserById(id);
     }
 
 }
---
+++ src/main/java/com/frotas/FirstProject/services/UserService.java
@@ -0,0 +1,50 @@
+package com.frotas.FirstProject.services;
+
+import com.frotas.FirstProject.model.User;
+import com.frotas.FirstProject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User addNewUser(User user){
+        String pws = user.getPassword();
+        user.setPassword(new BCryptPasswordEncoder().encode(pws));
+
+        return userRepository.save(user);
+    }
+
+    public Iterable<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    /**
+     * @param id id
+     * @return User by Id
+     */
+    public ResponseEntity<User> getUserById(Integer id){
+        return userRepository.findById(id).map(
+                        user -> ResponseEntity.ok().body(user))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
+/*    private String getUserAuthenticated(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(!(authentication instanceof AnonymousAuthenticationToken)){
+            String currentUsername = authentication.getName();
+            return currentUsername;
+        }
+        return "Não há usuário autenticado";
+    }*/
+
+}
