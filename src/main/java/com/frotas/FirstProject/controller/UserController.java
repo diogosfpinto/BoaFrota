1d22640ae8500cf6084191d8a2bba90e2784e805
diogosfpinto <diogo.sfpinto@gmail.com>
2022-04-03 22:54:29 -0400

    Documentando com swagger os recursos UserController e VehicleController.

--- src/main/java/com/frotas/FirstProject/controller/UserController.java
+++ src/main/java/com/frotas/FirstProject/controller/UserController.java
@@ -2,6 +2,9 @@ package com.frotas.FirstProject.controller;
 
 import com.frotas.FirstProject.model.User;
 import com.frotas.FirstProject.services.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,20 +17,37 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation("Criando novo usuário no sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Usuário criado com sucesso."),
+            @ApiResponse(code = 500,
+                    message = "Houve um erro ao criar novo usuário, verifique as informações.")
+    })
     @PostMapping(path="/user")
     @ResponseStatus(HttpStatus.CREATED)
     public @ResponseBody User addNewUser(@RequestBody User user){
         return userService.addNewUser(user);
     }
 
+    @ApiOperation("Retorna todos usuários cadastrados no sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Usuários retornados com sucesso."),
+            @ApiResponse(code = 500,
+                    message = "Houve um erro ao retornar todos os usuários, verifique as informações.")
+    })
     @GetMapping(path="/users")
     public @ResponseBody Iterable<User> getAllUser(){
         return userService.getAllUsers();
     }
 
+    @ApiOperation("Retorna usuário pelo ID informado como parâmetro.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Usuário retornado com sucesso."),
+            @ApiResponse(code = 500,
+                    message = "Houve um erro ao retornar o usuário por ID, verifique as informações.")
+    })
     @GetMapping(path="/users/{id}")
     public ResponseEntity<User> getUserById(@PathVariable (value = "id") Integer id){
         return userService.getUserById(id);
     }
-
 }
--- src/main/java/com/frotas/FirstProject/controller/VehicleController.java
+++ src/main/java/com/frotas/FirstProject/controller/VehicleController.java
@@ -2,6 +2,10 @@ package com.frotas.FirstProject.controller;
 
 import com.frotas.FirstProject.model.Vehicle;
 import com.frotas.FirstProject.services.VehicleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +18,12 @@ public class VehicleController {
     @Autowired
     private VehicleService vehicleService;
 
+    @ApiOperation("Adicionando novo veículo no sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Veículo adicionado com sucesso."),
+            @ApiResponse(code = 500,
+                    message = "Houve um erro ao adicionar novo veículo, verifique as informações.")
+    })
     @PostMapping(path="/vehicle")
     @ResponseStatus(HttpStatus.CREATED)
     public @ResponseBody String addNewVehicle(@RequestBody Vehicle vehicle){
@@ -22,17 +32,35 @@ public class VehicleController {
         return "saved";
     }
 
+    @ApiOperation("Retornando todos os veículos do sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Veículos retornados com sucesso."),
+            @ApiResponse(code = 500,
+                    message = "Houve um erro ao retornar todos os veículos, verifique as informações.")
+    })
     @GetMapping(path="/vehicles")
     public @ResponseBody Iterable<Vehicle> getAllVehicles(){
         return vehicleService.getAllVehicles();
     }
 
+    @ApiOperation("Retornando veículo por ID informado.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Veículo retornado com sucesso."),
+            @ApiResponse(code = 500,
+                    message = "Houve um erro ao retornar veículo por ID, verifique as informações.")
+    })
     @GetMapping(path = "/vehicle/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Vehicle> getVehicleById(@PathVariable (value = "id") Integer id){
         return vehicleService.getVehicleById(id);
     }
 
+    @ApiOperation("Atualizando dados de veículo por ID.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Veículo atualizado com sucesso."),
+            @ApiResponse(code = 500,
+                    message = "Houve um erro ao atualizar veículo, verifique as informações.")
+    })
     @PutMapping("/vehicle/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Vehicle> updateVehicleById(@PathVariable (value = "id") Integer id,
@@ -40,6 +68,12 @@ public class VehicleController {
         return vehicleService.updateVehicleById(id, vehicle);
     }
 
+    @ApiOperation("Excluindo veículo do sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Veículo excluído com sucesso."),
+            @ApiResponse(code = 500,
+                    message = "Houve um erro ao excluir veículo, verifique as informações.")
+    })
     @DeleteMapping("/vehicle/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Object> deleteVehiclebyId(@PathVariable (value = "id") Integer id){
